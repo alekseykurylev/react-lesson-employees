7ca0644770bee0f05a0ea535e43b2a3a796ce1f0
Aleksey Kurylev <alekseykurylev@gmail.com>
2022-05-19 19:13:02 +0300

    feat: добавляет нового сотрудника

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,18 +18,29 @@ class App extends Component {
         {name: 'Carl W.', salary: 5000, increase: false, id: 3}
       ]
     }
+    this.maxId = 4
   }
 
   deleteItem = (id) => {
     this.setState(({data}) => {
-      // const index = data.findIndex(elem => elem.id === id)
-
-      // const befor = data.slice(0, index)
-      // const after = data.slice(index + 1)
-      // const newArr = [...befor, ...after]
-
       return {
         data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name: name,
+      salary: +salary,
+      increase: false,
+      id: this.maxId++
+    }
+
+    this.setState(({data}) => {
+      const newArr = [...data, newItem]
+      return {
+        data: newArr
       }
     })
   }
@@ -46,7 +57,7 @@ class App extends Component {
           </div>
 
           <EmployeesList data={data} onDelete={this.deleteItem}/>
-          <EmployeesAddForm/>
+          <EmployeesAddForm onAdd={this.addItem}/>
       </div>
     )
   }
