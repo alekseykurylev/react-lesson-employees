e1000d1884dd1f7ff23ab43882b786437be33187
Aleksey Kurylev <alekseykurylev@gmail.com>
2022-05-20 20:51:35 +0300

    refactor: единый метод для переключения свойств сотрудника

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -18,11 +18,16 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault()
-        this.props.onAdd(this.state.name, this.state.salary)
-        this.setState({
-            name: '',
-            salary: ''
-        })
+        if(this.state.name.length >= 3 && this.state.salary > 0) {
+            this.props.onAdd(this.state.name, this.state.salary)
+            this.setState({
+                name: '',
+                salary: ''
+            })
+        } else {
+            alert('Введите корректные данные')
+        }
+
     }
 
     render() {
